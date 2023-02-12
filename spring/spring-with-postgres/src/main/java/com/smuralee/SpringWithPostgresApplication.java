89e99c9a829eb1920b4212a76d3e8c1410eba913
smuralee <suraj@surajmuraleedharan.com>
2023-02-12 13:51:30 +0000

    Added modules for Spring projects

---
+++ spring/spring-with-postgres/src/main/java/com/smuralee/SpringWithPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.smuralee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithPostgresApplication.class, args);
+	}
+
+}
