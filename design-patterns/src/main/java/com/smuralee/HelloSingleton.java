e17044f1e0f475f4c5f79c559848dfd29cb7d310
Suraj Muraleedharan <smuralee@users.noreply.github.com>
2024-02-28 15:17:07 +0000

    Update HelloSingleton.java

--- design-patterns/src/main/java/com/smuralee/HelloSingleton.java
+++ design-patterns/src/main/java/com/smuralee/HelloSingleton.java
@@ -1,21 +1,46 @@
 package com.smuralee;
+import java.io.Serializable;
 
-public class HelloSingleton {
+public class HelloSingleton implements Serializable {
 
-    private static HelloSingleton instance = null;
+    // Counter to keep track of the instance invocation
+    private static int instantiationCount = 0;
 
-    // Private constructor to avoid the public instantiation
+    // Instantiate as null
+    private static HelloSingleton obj = null;
+
     private HelloSingleton() {
+        // Keeps the constructor private, disallow instantiation outside the class
     }
 
-    public static HelloSingleton getInstance() {
-        if (instance == null) {
-            instance = new HelloSingleton();
+    public static void main(final String[] args) {
+        // Default execution of 1
+        int executionCount = 1;
+
+        // Extract the count if passed
+        if (args.length > 0) {
+            executionCount = Integer.parseInt(args[0]);
         }
-        return instance;
+
+        // Loop through the count
+        for (int i = 1; i <= executionCount; i++) {
+            HelloSingleton helloSingleton = getInstance();
+            helloSingleton.printMessage(i);
+        }
     }
 
-    public String buildMessage(final String message) {
-        return String.format("The response is - %s", message);
+    // getInstance for the singleton object
+    public static HelloSingleton getInstance() {
+        if (obj == null) {
+            obj = new HelloSingleton();
+            instantiationCount++;
+        }
+        return obj;
     }
-}
+
+    // Sample method for invocation
+    public void printMessage(final int executionCount) {
+        System.out.format(
+                "You have invoked the singleton instance. The instantiation count is %d and execution count is %d%n",
+                instantiationCount, executionCount);
+    }
